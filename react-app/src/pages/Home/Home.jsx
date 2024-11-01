0a55c2fb0fb71f7d06b8c2af1297ea1affcf9f53
Javier Ballón <javi4195@gmail.com>
2024-11-01 19:58:46 -0300

    Se cambio la forma de obtener el role del usuario y mostrarlo en Home

--- react-app/src/pages/Home/Home.jsx
+++ react-app/src/pages/Home/Home.jsx
@@ -6,12 +6,12 @@ export const Home = () => {
 
     const { roles } = useContext(AuthContext);
 
-    const hasRole = (roleName) => roles.includes(roleName);
+    const hasRole = (roleName) => roles === roleName;
 
     return (
         <div className="mx-auto">
             <div className="w-full container mx-auto py-14">
-                {hasRole('ROLE_ADMIN') && 
+                {hasRole('ADMIN') && 
                 <p>Bienvenido al sistema 
                     <button type="button" className="bg-gray-200 text-gray-800 mx-2 py-1 px-1 rounded items-center space-x-2" disabled>ADRMINISTRADOR</button>
                 </p>}
